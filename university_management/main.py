23d75503522beb51b05d28ae1b67419212bc23cd
Ruslan Yusupov <rbyusupov@gmail.com>
2023-06-25 18:01:05 +0300

    timestump 25_06

--- university_management/main.py
+++ university_management/main.py
@@ -3,59 +3,78 @@ from psycopg_pool import AsyncConnectionPool
 import uvicorn
 
 from config.config import MY_DSN_STRING
-from university_management.queries import students_list, Student, add_student
+from university_management import queries
 
 app = FastAPI()
+
 
 @app.on_event("startup")
 def open_pool():
     """create database connection pool"""
     app.state.pool = AsyncConnectionPool(MY_DSN_STRING, max_size=500)
 
+
 @app.on_event("shutdown")
 async def close_pool():
     """close database connection pool"""
     await app.state.pool.close()
 
+
 @app.get("/")
 async def index():
-    return await students_list(app.state.pool)
+    return await queries.students_list(app.state.pool)
+
 
 @app.post("/students/")
-async def post_students(student: Student):
-    return await add_student(pool=app.state.pool, student=student)
+async def post_students(student: queries.Student):
+    """Add a new student"""
+    return await queries.add_student(pool=app.state.pool, student=student)
+
 
 @app.get("/students/{student_id}")
 async def get_student(student_id: int):
-    return student_id
+    """Get a student by ID"""
+    return await queries.get_student(pool=app.state.pool, student_id=student_id)
+
 
 @app.put("/students/{student_id}")
-async def put_student(student_id: int):
-    return student_id
+async def put_student(student_id: int, student: queries.Student):
+    """Edit student information"""
+    return await queries.put_student(
+        pool=app.state.pool, student_id=student_id, student=student
+    )
+
 
 @app.delete("/students/{student_id}")
 async def delete_student(student_id: int):
-    return student_id
+    """Delete a student"""
+    return queries.delete_student(pool=app.state.pool, student_id=student_id)
+
 
 @app.get("/teachers/")
 async def get_teachers():
     pass
 
+
 @app.post("/courses/")
 async def post_course():
     pass
+
 
 @app.get("/courses/{course_id}")
 async def get_course():
     pass
 
+
 @app.get("/courses/{course_id}/students")
 async def get_students_by_course():
     pass
 
+
 @app.post("/grades/")
 async def post_grade():
     pass
+
 
 @app.put("/grades/{grade_id}")
 async def put_grade():
--- university_management/queries.py
+++ university_management/queries.py
@@ -1,29 +1,76 @@
 from psycopg_pool import AsyncConnectionPool
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 from psycopg.rows import class_row
 
 
 class Student(BaseModel):
+    # id: int
     full_name: str
     group_name: str
-        
+    # created_at: datetime = Field(default_factory=datetime.utcnow)
+
+    class Config:
+        default_readable_fields = {"full_neme", "group_name"}
+
+
 async def students_list(pool: AsyncConnectionPool):
     async with pool.connection() as conn:
         async with conn.cursor(row_factory=class_row(Student)) as cur:
             await cur.execute("SELECT * FROM students")
             rows = await cur.fetchall()
-            return {"students": rows}
-        
+            return rows
+
 
 async def add_student(pool: AsyncConnectionPool, student: Student):
     async with pool.connection() as conn:
         async with conn.cursor(row_factory=class_row(Student)) as cur:
-            await cur.execute("INSERT INTO students (full_name, created_at, group_name) VALUES ('{name}', '{created}', '{group_name}')".format(
-                name=student.full_name,
-                created=datetime.now(),
-                group_name=student.group_name                
-                ))
+            await cur.execute(
+                "INSERT INTO students (full_name, created_at, group_name) VALUES ('{name}', '{created}', '{group_name}')".format(
+                    name=student.full_name,
+                    created=datetime.now(),
+                    group_name=student.group_name,
+                )
+            )
             await cur.execute("SELECT * FROM students")
             rows = await cur.fetchall()
-            return {"students": rows}
+            return rows
+
+
+async def get_student(pool: AsyncConnectionPool, student_id: int):
+    async with pool.connection() as conn:
+        async with conn.cursor(row_factory=class_row(Student)) as cur:
+            await cur.execute(
+                "SELECT * FROM students WHERE id = {student_id}".format(
+                    student_id=student_id
+                )
+            )
+            rows = await cur.fetchone()
+            return rows
+
+
+async def put_student(pool: AsyncConnectionPool, student_id: int, student: Student):
+    async with pool.connection() as conn:
+        async with conn.cursor(row_factory=class_row(Student)) as cur:
+            await cur.execute(
+                "UPDATE students SET full_name = '{full_name}', group_name = '{group_name}' WHERE id = {student_id}".format(
+                    full_name=student.full_name,
+                    group_name=student.group_name,
+                    student_id=student_id,
+                )
+            )
+            rows = await cur.fetchall()
+            return rows
+
+
+async def delete_student(pool: AsyncConnectionPool, student_id: int):
+    async with pool.connection() as conn:
+        async with conn.cursor(row_factory=class_row(Student)) as cur:
+            await cur.execute(
+                "DELETE FROM students WHERE id = {student_id}".format(
+                    student_id=student_id
+                )
+            )
+            await cur.execute("SELECT * FROM students")
+            rows = await cur.fetchall()
+            return rows
